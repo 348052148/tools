b07864bae450a8c39e105490e87d3e20eba8a479
云淡风轻 <msbox@live.cn>
2017-11-11 16:16:21 +0800

    layer

--- content.js
+++ content.js
@@ -1,1 +1,54 @@
 //$('#page-wrapper').html("");
+
+var i = false;
+var cavans = $("<div id='canvas' ></div>");
+$("body").append(cavans);
+$("body").mousemove(function (event) {
+    if(i){
+        var top = event.clientX + document.body.scrollLeft - document.body.clientLeft;
+        var left =event.clientY + document.body.scrollTop - document.body.clientTop;
+        cavans.css({"top":event.pageY-cavans.height()/2});
+        cavans.css({"left":event.pageX-cavans.width()/2});
+    }
+});
+
+cavans.mouseup(function () {
+    i=false;
+});
+cavans.mousedown(function () {
+    i=true;
+});
+cavans.click(function () {
+        layer.msg('hello');
+});
+
+var pWidth = $('body').width();
+var pHeight = window.innerHeight;
+
+cavans.css({'top':pHeight-100,'left':pWidth-100});
+
+BGame.createWindow(0,0,60,60,'1px solid #eee',document.getElementById('canvas'));
+BGame.init();
+var director= new BDirector();
+director.init(BGame);
+
+var pinpinScence = new BScene();
+
+var explorer = new BExplorer(director);
+
+explorer.loadImages({
+    pingping: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2020551558,2975581821&fm=27&gp=0.jpg"
+
+},function (loadedImages,numImages) { //加载中回调
+
+},function () { //加载完成回调
+    var PingSprite = new BImageSprite();
+    PingSprite.setImageData(explorer.IMG['pingping']);
+    PingSprite.height=60;
+    PingSprite.width=60;
+    pinpinScence.addChild(PingSprite);
+
+});
+
+director.addScene(pinpinScence);
+director.run();
--- popup.js
+++ popup.js
@@ -18,12 +18,15 @@ window.onload=function () {
             msg_fps:3, //消息 频率
             nickname: '来自老公的关心',
             currentPage:1,
-            qmessage:null
+            qmessage:null, // 消息对象
+            robot:null, //机器人对象
+            problem:'',
+            robot_info_list:[]
         },
         created:function () {
             if(this.currentPage == 1) this.title = '聊天';
             if(this.currentPage == 2) this.title = '音乐';
-            if(this.currentPage == 3) this.title = '时间';
+            if(this.currentPage == 3) this.title = '问答';
             if(this.currentPage == 4) this.title = '更多';
             if(this.currentPage == 5) this.title = '设置';
 
@@ -41,6 +44,8 @@ window.onload=function () {
                 this.nickname = localStorage.nickname;
             }
 
+            this.robot = new Robot();
+
             this.qmessage = new Message();
         },
         methods:{
@@ -48,6 +53,29 @@ window.onload=function () {
                 this.qmessage.sendMessage(this.id,this.nickname,this.message,function (data) {
                     Vm.message = '';
                 });
+            },
+            sendQuestion:function () {
+                this.robot.chatMessage(this.problem,function (data) {
+                    if(data.code == 100000){
+                        Vm.robot_info_list.push({type:'text',info:data.text});
+                    }
+                    if(data.code == 200000){
+                        Vm.robot_info_list.push({type:'url',info:data.text,url:data.url});
+                    }
+                    if(data.code == 302000){
+                        Vm.robot_info_list.push({type:'news',info:data.text,list:data.list});
+                    }
+                    if(data.code == 308000){
+                        Vm.robot_info_list.push({type:'food',info:data.text,list:data.list});
+                    }
+
+                });
+            },
+            remove_robot:function (index) {
+                this.robot_info_list.splice(index,1);
+            },
+            openLink:function (url) {
+                chrome.tabs.create({url:url});
             },
             setAttr:function () {
                 localStorage.timer = this.timer*1000*60;
@@ -66,7 +94,7 @@ window.onload=function () {
                 this.currentPage = key;
                 if(this.currentPage == 1) this.title = '聊天';
                 if(this.currentPage == 2) this.title = '音乐';
-                if(this.currentPage == 3) this.title = '时间';
+                if(this.currentPage == 3) this.title = '问答';
                 if(this.currentPage == 4) this.title = '更多';
                 if(this.currentPage == 5) this.title = '设置';
             }
